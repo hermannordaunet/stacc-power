3530f4dd8208b3b48872623ca39d2bcb013bcde8
hermanno <hermanno@uio.no>
2023-03-08 21:47:11 +0100

    added a text for the idea behind the design part

--- pages/api/spot.js
+++ pages/api/spot.js
@@ -2,7 +2,9 @@ import spotData from "../../data/localization.json";
 import { convertDate } from "../utils"
 
 export default function handler(req, res) {
-
+    spotData["Spotpris per time"].forEach(item => {
+        item.from = convertDate(item.from);
+    });
 
     res.status(200).json(spotData);
 };
