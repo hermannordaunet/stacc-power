3c22c6c8e97fcb9808e0235cb24fdbeda710e348
hermanno <hermanno@uio.no>
2023-03-13 20:39:52 +0100

    fixing more

--- pages/api/consumption/range/[...range].js
+++ pages/api/consumption/range/[...range].js
@@ -1,11 +1,14 @@
 import consumption from "../../../../data/consumption.json";
-import {
-    setCorrectDateVariables,
-    checkIfRangeInsideRange,
-} from "../../utils";
+import { setCorrectDateVariables, checkIfRangeInsideRange } from "../../utils";
 
 export default function handler(req, res) {
     var { range } = req.query;
+
+    if (range.length < 2) {
+        res.status(400).json({
+            error: "query needs to be /YYYY-MM-DDTHH/YYYY-MM-DDTHH ",
+        });
+    }
 
     if (range[0].length < 13) {
         res.status(400).json({
@@ -19,34 +22,26 @@ export default function handler(req, res) {
         });
     }
 
-    if (range[0] && range[1]) {
-        var [startDate, endDate] = setCorrectDateVariables(range);
+    var [startDate, endDate] = setCorrectDateVariables(range);
 
-        checkIfRangeInsideRange(res, consumption, startDate, endDate);
+    checkIfRangeInsideRange(res, consumption, startDate, endDate);
 
-        const filteredData = consumption.filter((data) => {
-            const fromDate = data.from.substring(0, 13);
-            return fromDate >= startDate && fromDate <= endDate;
-        });
+    const filteredData = consumption.filter((data) => {
+        const fromDate = data.from.substring(0, 13);
+        return fromDate >= startDate && fromDate <= endDate;
+    });
 
-        // Calculate the total consumption for that range.
-        const totalRangeConsumption = parseFloat(
-            filteredData
-                .reduce((acc, cur) => acc + cur.consumption, 0)
-                .toFixed(3)
-        );
+    // Calculate the total consumption for that range.
+    const totalRangeConsumption = parseFloat(
+        filteredData.reduce((acc, cur) => acc + cur.consumption, 0).toFixed(3)
+    );
 
-        var response = {
-            totalRangeConsumption,
-            startDate,
-            endDate,
-            filteredData,
-        };
+    var response = {
+        totalRangeConsumption,
+        startDate,
+        endDate,
+        filteredData,
+    };
 
-        res.status(200).json(response);
-    } else {
-        res.status(400).json({
-            error: "query needs to be /YYYY-MM-DDTHH/YYYY-MM-DDTHH ",
-        });
-    }
+    res.status(200).json(response);
 }
