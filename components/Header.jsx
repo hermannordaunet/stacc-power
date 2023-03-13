68ae706fdaa014dc4bc4386edd80424aef999255
hermanno <hermanno@uio.no>
2023-03-13 21:36:20 +0100

    added message to index.js

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,9 @@
 import styles from "./Header.module.css";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>Price</h1>
-      <h1>Comparison </h1>
-
+      <h1>This is not were my work is done. I focused on API endpoints</h1>
     </header>
   );
 }
